c04fa6280411643db6a863e9ec9a792c0566c56d
Olivier Van Acker <cyberroadie@gmail.com>
2015-08-06 16:18:44 +0100

    added simple SCTP echo client/server

---
+++ simple_echo/c/sctp/client/simpleEchoSCTPClient.c
@@ -0,0 +1,111 @@
+#include "simpleEchoSCTPClient.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netinet/sctp.h>
+
+#define RECVBUFSIZE 2048
+
+
+int main() {
+    char *message = "hello";
+
+    int sd = socket(AF_INET, SOCK_SEQPACKET, IPPROTO_SCTP);
+    if(sd == -1) {
+        perror("failure opening socket");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sctp_initmsg initmsg = {0};
+    initmsg.sinit_num_ostreams = 1;
+    int s = setsockopt(sd, IPPROTO_SCTP, SCTP_INITMSG, &initmsg, sizeof(initmsg));
+    if(s == -1) {
+        perror("error setting socket options");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in address;
+    bzero((void*)&address, sizeof(address));
+    address.sin_family = AF_INET;
+    address.sin_port = htons(4242);
+    char *host = "127.0.0.1";
+    address.sin_addr.s_addr = inet_addr(host);
+    address.sin_len = (socklen_t) sizeof(struct sockaddr_in);
+
+    // Structure containing data
+    struct iovec iov;
+    bzero(&iov, sizeof(iov));
+    iov.iov_base = message;
+    iov.iov_len = strlen(message);
+
+    struct sctp_sndinfo sinfo;
+    bzero(&sinfo, sizeof(struct sctp_sndinfo));
+
+    // Send information structure
+    sinfo.snd_sid = 1;
+    sinfo.snd_ppid = htonl(424242);
+    //sinfo.snd_flags = SCTP_UNORDERED;
+
+    // receiving variables
+    struct iovec echoiov[1];
+    char buf[RECVBUFSIZE];
+    echoiov->iov_base = buf;
+    echoiov->iov_len = RECVBUFSIZE;
+
+    struct sockaddr_in from;
+    socklen_t *fromlen = NULL, infolen;
+    int flags = 0;
+    unsigned int infotype = 0;
+    struct sctp_rcvinfo rinfo;
+    bzero(&rinfo, sizeof(struct sctp_rcvinfo));
+    infolen = sizeof(rinfo);
+
+    struct sctp_initmsg initMsg;
+    bzero((void *)&initMsg, sizeof(initMsg));
+    initMsg.sinit_num_ostreams = 100;
+    initMsg.sinit_max_instreams = 0;
+    initMsg.sinit_max_attempts = 0;
+    initMsg.sinit_max_init_timeo = 0;
+
+    setsockopt(sd, IPPROTO_SCTP, SCTP_INITMSG, &initMsg, sizeof(initMsg));
+
+
+    int n = sctp_sendv(sd,
+                       &iov,
+                       1,
+                       (struct sockaddr *) &address,
+                       1,
+                       (void *) &sinfo,
+                       sizeof(struct sctp_sndinfo),
+                       SCTP_SENDV_SNDINFO,
+                       0);
+
+    if(n == -1) {
+        perror("failure sending message");
+        exit(EXIT_FAILURE);
+    }
+
+    int length = sctp_recvv(sd,
+                            echoiov,
+                            1,
+                            (struct sockaddr *) &from,
+                            fromlen,
+                            &rinfo,
+                            &infolen,
+                            &infotype,
+                            &flags);
+
+    if(length == -1) {
+        perror("failure receiving message");
+        exit(EXIT_FAILURE);
+    } else {
+        printf("received: %s\n", buf);
+    }
+
+    return EXIT_SUCCESS;
+}
+
---
+++ simple_echo/c/sctp/server/simpleEchoSCTPServer.c
@@ -0,0 +1,130 @@
+#include "simpleEchoSCTPServer.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/sctp.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define RECVBUFSIZE 10
+
+int main() {
+
+    int sd;
+
+    sd = socket(PF_INET, SOCK_SEQPACKET, IPPROTO_SCTP);
+
+    if(sd == -1) {
+        perror("failure opening socket");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in serverAddress;
+    bzero((void *)&serverAddress, sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverAddress.sin_port = htons(4242);
+
+    int rb = bind(sd, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
+    if(rb == -1){
+        perror("bind error");
+        close(sd);
+        exit(EXIT_FAILURE);
+    }
+
+    struct sctp_initmsg initMsg;
+    bzero((void *)&initMsg, sizeof(initMsg));
+    initMsg.sinit_num_ostreams = 100;
+    initMsg.sinit_max_instreams = 100;
+    initMsg.sinit_max_attempts = 0;
+    initMsg.sinit_max_init_timeo = 0;
+
+    setsockopt(sd, IPPROTO_SCTP, SCTP_INITMSG, &initMsg, sizeof(initMsg));
+
+    const int on = 1;
+    setsockopt(sd, IPPROTO_SCTP, SCTP_LISTEN_FIX, &on, sizeof(int));
+
+    int rl = listen(sd, 1);
+    if(rl < 0) {
+        perror("failed to listen for connection");
+        close(sd);
+        exit(EXIT_FAILURE);
+    }
+
+    struct iovec iov[1];
+    char buf[RECVBUFSIZE];
+    iov->iov_base = buf;
+    iov->iov_len = RECVBUFSIZE;
+
+    if (setsockopt(sd, IPPROTO_SCTP, SCTP_RECVRCVINFO, &on, sizeof(on)) < 0) {
+        perror("setsockopt SCTP_RECVRCVINFO");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sctp_sndinfo sinfo;
+    bzero(&sinfo, sizeof(struct sctp_sndinfo));
+
+
+    for(;;) {
+        struct sockaddr_in  client_addr;
+        bzero((void*)&client_addr, sizeof(client_addr));
+        socklen_t fromlen, infolen;
+        int flags = 0;
+        unsigned int infotype = 0;
+        struct sctp_rcvinfo rinfo;
+        bzero(&rinfo, sizeof(struct sctp_rcvinfo));
+        infolen = sizeof(rinfo);
+        fromlen = sizeof(client_addr);
+
+        int length = sctp_recvv(sd,
+                                iov,
+                                1,
+                                (struct sockaddr *) &client_addr,
+                                &fromlen,
+                                &rinfo,
+                                &infolen,
+                                &infotype,
+                                &flags);
+
+        if(length == -1) {
+            perror("error receiving message: ");
+            exit(EXIT_FAILURE);
+        }
+
+        buf[length] = '\0';
+        printf("message received: %s\n", buf);
+
+        sinfo.snd_sid = rinfo.rcv_sid;
+        sinfo.snd_flags = SCTP_UNORDERED;
+        sinfo.snd_ppid = rinfo.rcv_ppid;
+        sinfo.snd_assoc_id = rinfo.rcv_assoc_id;
+
+        printf("sending message back: %s\n", buf);
+
+        char str[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &client_addr.sin_addr, str, sizeof(str));
+        printf("On address %s\n", str);
+
+        int n = sctp_sendv(sd,
+                           iov,
+                           1,
+                           (struct sockaddr *) &client_addr,
+                           1,
+                           (void *) &sinfo,
+                           sizeof(struct sctp_sndinfo),
+                           SCTP_SENDV_SNDINFO,
+                           0);
+        if(n == -1) {
+            perror("error sending message");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("message sent\n");
+
+    }
+
+    return EXIT_SUCCESS;
+}
